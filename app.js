a3fb3032922d517723b178033a2ea6716a3591bb
Saad Mirza <122702367+saadmirza729@users.noreply.github.com>
2025-08-08 01:48:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// Chapter # 1 ------
+
+// Solution of Question # 1
+
+alert("Hello! Welcome to my website")
+
+// Solution of Question # 2
+
+alert("Error! Please enter a valid password")
+
+// Solution of Question # 3
+
+
+
