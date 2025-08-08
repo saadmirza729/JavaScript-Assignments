eb9e92c6c0596088b83fa2f88c68974b9acad103
Saad Mirza <saadmirza729@gmail.com>
2025-08-08 18:22:47 +0500

    js updated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Chapter # 1 ------
 
+
 // Solution of Question # 1
 
 alert("Hello! Welcome to my website")
@@ -16,6 +17,89 @@ alert("Welcome to JS Land... \nHappy Coding!")
 
 alert("Welcome to JS Land");
 alert("Happy Coding!");
+
+// Solution of Question # 5
+// Done
+
+// Solution of Question # 6
+
+alert("welcome to my website")
+
+// Solution of Question # 7
+// Done
+
+  // --------------------------------------------------------------------------
+
+// Chapter # 2 ------
+
+
+// Solution of Question # 1
+
+var username;
+
+// Solution of Question # 2
+
+var myname = "Saad Mirza";
+
+// Solution of Question # 3
+
+var message = "Hello World";
+alert(message);
+
+// Solution of Question # 4
+
+var studentname = "Jhone Doe";
+var studentage = "15 years old";
+var studentcourse = "Certified Mobile Application Development";
+
+alert(studentname);
+alert(studentage);
+alert(studentcourse);
+
+// Solution of Question # 5
+
+var pizza = "Pizza \nPizz \nPiz \nPi \nP";
+alert(pizza);
+
+// Solution of Question # 6
+
+var name1 = "saad";
+var atmail = "@gmail.com";
+var email = name1 + atmail;
+alert(email);
+
+// Solution of Question # 7
+
+var book1 = "I am trying to learn from the book";
+var book2 = "A smarter way to learn JavaScript";
+var book = book1 + " " + book2;
+alert(book)
+
+// Solution of Question # 8
+
+document.write("Yah! I can write HTML content Through JS")
+
+// Solution of Question # 9
+
+
+
+// --------------------------------------------------------------------------
+
+// Chapter # 3 ------
+
+
+// Solution of Question # 1
+
+
+var age = "I am 15 years old";
+alert(age);
+
+// Solution of Question # 2
+
+var visits = "you have visited this site 14 times";
+alert(visits);
+
+
 
 
 
