fc5b9172a992f637a1acac3dba06779d193ec242
Saad Mirza <saadmirza729@gmail.com>
2025-08-11 15:40:21 +0500

    JS Assignments updated

--- app.js
+++ app.js
@@ -3,27 +3,27 @@
 
 // Solution of Question # 1
 
-alert("Hello! Welcome to my website")
+// alert("Hello! Welcome to my website")
 
 // Solution of Question # 2
 
-alert("Error! Please enter a valid password")
+// alert("Error! Please enter a valid password")
 
 // Solution of Question # 3
 
-alert("Welcome to JS Land... \nHappy Coding!")
+// alert("Welcome to JS Land... \nHappy Coding!")
 
 // Solution of Question # 4
 
-alert("Welcome to JS Land");
-alert("Happy Coding!");
+// alert("Welcome to JS Land");
+// alert("Happy Coding!");
 
 // Solution of Question # 5
 // Done
 
 // Solution of Question # 6
 
-alert("welcome to my website")
+// alert("welcome to my website")
 
 // Solution of Question # 7
 // Done
@@ -35,49 +35,49 @@ alert("welcome to my website")
 
 // Solution of Question # 1
 
-var username;
+// var username;
 
 // Solution of Question # 2
 
-var myname = "Saad Mirza";
+// var myname = "Saad Mirza";
 
 // Solution of Question # 3
 
-var message = "Hello World";
-alert(message);
+// var message = "Hello World";
+// alert(message);
 
 // Solution of Question # 4
 
-var studentname = "Jhone Doe";
-var studentage = "15 years old";
-var studentcourse = "Certified Mobile Application Development";
+// var studentname = "Jhone Doe";
+// var studentage = "15 years old";
+// var studentcourse = "Certified Mobile Application Development";
 
-alert(studentname);
-alert(studentage);
-alert(studentcourse);
+// alert(studentname);
+// alert(studentage);
+// alert(studentcourse);
 
 // Solution of Question # 5
 
-var pizza = "Pizza \nPizz \nPiz \nPi \nP";
-alert(pizza);
+// var pizza = "Pizza \nPizz \nPiz \nPi \nP";
+// alert(pizza);
 
 // Solution of Question # 6
 
-var name1 = "saad";
-var atmail = "@gmail.com";
-var email = name1 + atmail;
-alert(email);
+// var name1 = "saad";
+// var atmail = "@gmail.com";
+// var email = name1 + atmail;
+// alert(email);
 
 // Solution of Question # 7
 
-var book1 = "I am trying to learn from the book";
-var book2 = "A smarter way to learn JavaScript";
-var book = book1 + " " + book2;
-alert(book)
+// var book1 = "I am trying to learn from the book";
+// var book2 = "A smarter way to learn JavaScript";
+// var book = book1 + " " + book2;
+// alert(book)
 
 // Solution of Question # 8
 
-document.write("Yah! I can write HTML content Through JS")
+// document.write("Yah! I can write HTML content Through JS")
 
 // Solution of Question # 9
 
@@ -91,13 +91,57 @@ document.write("Yah! I can write HTML content Through JS")
 // Solution of Question # 1
 
 
-var age = "I am 15 years old";
-alert(age);
+// var age = "I am 15 years old";
+// alert(age);
 
 // Solution of Question # 2
 
-var visits = "you have visited this site 14 times";
-alert(visits);
+// var visits = "you have visited this site 14 times";
+// alert(visits);
+
+// Solution of Question # 3
+
+// var word1 = "My birth year is";
+// var birthyear = "1990";
+// var word2 = "Data type of my declared variable is number";
+
+// document.write(word1 + " " + birthyear + "<br>" + word2);
+
+// Solution of Question # 4
+
+// var VisitorName = "John Doe";
+// var product = "T-shirts";
+// var items = 5
+
+// document.write(VisitorName + " " + "ordered" + " " + items + " " + product +" " + "on XYZ Clothing store" )
+
+
+// --------------------------------------------------------------------------
+
+// Chapter # 4 ------
+
+
+// Solution of Question # 1
+
+// var MyFirstName = "Saad" , MyLastName = "Mirza" , MyFullName = "Saad Mirza"
+
+// Solution of Question # 2
+// Legal Variable Names ----
+
+// var userName , _totalAmount , $price , age2 , camelCaseVar;
+
+// illegal ----
+
+// var 2ndName , my-name , first name , @user , user! , my var ;
+
+// document.write("<h2> Rules for naming JS variable </h2>")
+
+// document.write("Variable names can only contain, numbers, $ and _ .For example : $my_1stVariable");
+// document.write("<br> Variable must begin with a letter, $ or _ .For example : $name, _name or name");
+// document.write("<br> Variable names are case sensitive");
+// document.write("<br> Variable names should not be JS keywords");
+
+
 
 
 
